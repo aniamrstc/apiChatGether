b3b170c09a6c3ccac090ffab507ffc38cf0b88ef
aniamrstc <ania.mrstc@eduge.ch>
2023-02-08 16:36:10 +0100

    fonction d'ajout et de recuperation du nombre de message par user

--- Function_Database.php
+++ Function_Database.php
@@ -1,28 +1,44 @@
 <?php
 
 require_once("./Database.php");
-function selectAllCocktail()
+function selectAllMessage()
 {
     $conn = db();
 
-    $query = $conn->prepare("SELECT * FROM Cocktails");
+    $query = $conn->prepare("SELECT * FROM Messages");
     $query->execute();
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
     return $result;
-    
 }
 function selectSpecificMessage($numEmetteur, $numRecepteur)
 {
     $conn = db();
 
     $query = $conn->prepare("SELECT * FROM Messages WHERE NumEmetteur = ? AND NumRecepteur = ? OR NumRecepteur = ? AND NumEmetteur = ?");
-   // $query->bindParam('cocktail',$cocktail,PDO::PARAM_STR);
-    $query->execute([$num,$num]);
+    // $query->bindParam('cocktail',$cocktail,PDO::PARAM_STR);
+    $query->execute([$numEmetteur, $numRecepteur]);
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
     return $result;
-    
+}
+function InsertUser($numUser)
+{
+
+    $conn = db();
+    $query = $conn->prepare("INSERT INTO Users (Num) value(?)");
+    $query->execute([$numUser]);
 }
 
+function InsertMessage($numEmetteur, $numRecepteur, $Message)
+{
+    $conn = db();
+    $query = $conn->prepare("INSERT INTO Messages (NumEmetteur,NumRecepteur,Message) value(?,?,?)");
+    $query->execute([$numEmetteur, $numRecepteur, $Message]);
+}
 
-
-?>
+function GetNumberOfMessageForAUser($numUser)
+{
+    $conn = db();
+    $query = $conn->prepare("SELECT COUNT(Num) FROM Messages,Users WHERE Messages.numUser=Users.Num AND Users.Num=? ");
+    $query->execute([$numUser]);
+    return $query->fetch(PDO::FETCH_NUM)[0];
+}
